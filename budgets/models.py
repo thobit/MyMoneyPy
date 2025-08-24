70b74cd6421d8351b78c81c985cf4ad6087b6803
Thorsten Bittner <thorsten.bittner@balticbytes.com>
2025-08-24 20:10:00 +0200

    added model for transactions

--- budgets/models.py
+++ budgets/models.py
@@ -1,11 +1,28 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 
 class Budget(models.Model):
     name = models.CharField(max_length = 64,
                             default = 'Main Budget')
-    current_balance = models.DecimalField(max_digits = 64,
+    current_balance = models.DecimalField(max_digits = 24,
                                           decimal_places = 6,
                                           default = 0)
+    target_balance = models.DecimalField(max_digits = 24,
+                                         decimal_places = 6,
+                                         null = True)
+
+
+class BudgetTransaction(models.Model):
+    tx_id = models.UUIDField(primary_key = True,
+                             default = uuid.uuid4,
+                             editable = False)
+    amount = models.DecimalField(max_digits = 24,
+                                 decimal_places = 6,
+                                 default = 0)
+    occured_at = models.DateTimeField(auto_now_add = True,
+                                      db_index = True)
+    description = models.CharField(max_length = 128,
+                                   default = "money goes brrrr")
 
