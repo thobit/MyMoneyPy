e4cec3b8d2e23866d5bd242b5db83209c1f55c95
Thorsten Bittner <thorsten.bittner@balticbytes.com>
2025-08-24 19:20:47 +0200

    changed field type of name

--- budgets/models.py
+++ budgets/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Budget(models.Model):
-    name = models.TextField(max_length = 64,
+    name = models.CharField(max_length = 64,
                             default = 'Main Budget')
     current_balance = models.DecimalField(max_digits = 64,
                                           decimal_places = 6,
