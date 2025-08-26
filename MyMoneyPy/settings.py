e6ffca11e14134734215acc6ec3a9b711847fa22
Thorsten Bittner <thorsten.bittner@balticbytes.com>
2025-08-26 19:30:50 +0200

    as always, forgot the migrations

--- MyMoneyPy/settings.py
+++ MyMoneyPy/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'budgets',
+    'transactions',
     'accounts',
     'graphene_django',
     'django.contrib.admin',
--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Transaction(models.Model):
+    tx_id = models.UUIDField(primary_key = True,
+                             default = uuid.uuid4,
+                             editable = False)
+    amount = models.DecimalField(max_digits = 24,
+                                 decimal_places = 6)
+    tx_budget = models.ForeignKey("budgets.Budget",
+                                  on_delete = models.SET_NULL,
+                                  null = True,
+                                  blank = True)
+    tx_account = models.ForeignKey("accounts.Account",
+                                   on_delete = models.SET_NULL,
+                                   null = True,
+                                   blank = True)
