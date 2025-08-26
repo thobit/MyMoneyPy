66df36d0015ec0e4ad52ec5f2209ac97df809ee7
Thorsten Bittner <thorsten.bittner@balticbytes.com>
2025-08-26 18:25:39 +0200

    removed transaction model from budget model

--- budgets/models.py
+++ budgets/models.py
@@ -13,16 +13,3 @@ class Budget(models.Model):
                                          decimal_places = 6,
                                          null = True)
 
-
-class BudgetTransaction(models.Model):
-    tx_id = models.UUIDField(primary_key = True,
-                             default = uuid.uuid4,
-                             editable = False)
-    amount = models.DecimalField(max_digits = 24,
-                                 decimal_places = 6,
-                                 default = 0)
-    occured_at = models.DateTimeField(auto_now_add = True,
-                                      db_index = True)
-    description = models.CharField(max_length = 128,
-                                   default = "money goes brrrr")
-
